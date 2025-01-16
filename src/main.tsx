cfdcc603886eb3240ae304f1cc939c2257b6af7f
mhollink <hollink.mh@gmail.com>
2025-01-16 13:50:38 +0100

    Set up firebase with auth context

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,20 @@ import { HelmetProvider } from "react-helmet-async";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ErrorBoundary } from "./components/error-boundary/ErrorBoundary.tsx";
 import { RootFallback } from "./components/error-boundary/RootFallback.tsx";
+import { AuthProvider } from "./firebase/FirebaseAuthContext.tsx";
 import { routes } from "./routing/routes.tsx";
 import { ThemeContextProvider } from "./theme/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary inCaseOfError={<RootFallback />}>
-      <ThemeContextProvider>
-        <HelmetProvider>
-          <RouterProvider router={createBrowserRouter(routes)} />
-        </HelmetProvider>
-      </ThemeContextProvider>
+      <AuthProvider>
+        <ThemeContextProvider>
+          <HelmetProvider>
+            <RouterProvider router={createBrowserRouter(routes)} />
+          </HelmetProvider>
+        </ThemeContextProvider>
+      </AuthProvider>
     </ErrorBoundary>
   </StrictMode>,
 );
