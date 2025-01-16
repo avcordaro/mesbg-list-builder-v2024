7c7b73d13da9bcb2c51eed3f7fce7531ff4c2ec1
mhollink <hollink.mh@gmail.com>
2025-01-16 13:50:38 +0100

    Set up firebase with auth context

---
+++ src/firebase/FirebaseAuthContext.tsx
@@ -0,0 +1,60 @@
+import { getIdToken, onAuthStateChanged, User } from "firebase/auth";
+import {
+  createContext,
+  FunctionComponent,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { auth } from "./config";
+
+// Define types for the AuthContext
+interface AuthContextProps {
+  user: User | null;
+  idToken: string | null;
+  loading: boolean;
+}
+
+// Define the default state of AuthContext
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export const AuthProvider: FunctionComponent<PropsWithChildren> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [idToken, setIdToken] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    // Listen to user authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      setUser(firebaseUser);
+      if (firebaseUser) {
+        const token = await getIdToken(firebaseUser);
+        setIdToken(token);
+      } else {
+        setIdToken(null);
+      }
+      setLoading(false);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, idToken, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
