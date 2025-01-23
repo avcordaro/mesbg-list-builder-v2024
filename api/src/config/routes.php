435d22d29b8c9fa7a26baeaff713ce4c67d9fa59
mhollink <hollink.mh@gmail.com>
2025-01-23 21:37:28 +0100

    Fixup routes

--- api/src/config/routes.php
+++ api/src/config/routes.php
@@ -8,41 +8,27 @@ use MLB\matches\GameController;
 use MLB\rosters\RosterController;
 use Slim\Routing\RouteCollectorProxy;
 
-
 $app->group("/v2024", function (RouteCollectorProxy $v2024) {
-
     $v2024->group("/rosters", function (RouteCollectorProxy $rosters) {
-
         $rosters->post("", [RosterController::class, 'create'])->add(TokenMiddleware::class);
         $rosters->get("", [RosterController::class, 'findAll'])->add(TokenMiddleware::class);
         $rosters->get("/{rosterId}", [RosterController::class, 'find'])->add(TokenMiddleware::class);
         $rosters->put("/{rosterId}", [RosterController::class, 'update'])->add(TokenMiddleware::class);
         $rosters->delete("/{rosterId}", [RosterController::class, 'delete'])->add(TokenMiddleware::class);
-
     });
-
     $v2024->group("/games", function (RouteCollectorProxy $games) {
-
         $games->post("", [GameController::class, 'create'])->add(TokenMiddleware::class);
         $games->get("", [GameController::class, 'read'])->add(TokenMiddleware::class);
         $games->put("/{gameId}", [GameController::class, 'update'])->add(TokenMiddleware::class);
         $games->delete("/{gameId}", [GameController::class, 'delete'])->add(TokenMiddleware::class);
-
     });
-
     $v2024->group("/collection", function (RouteCollectorProxy $collection) {
-
         $collection->get("", [CollectionController::class, 'get'])->add(TokenMiddleware::class);
         $collection->put("/{origin}/{model}", [CollectionController::class, 'upsert'])->add(TokenMiddleware::class);
         $collection->delete("/{origin}/{model}", [CollectionController::class, 'remove'])->add(TokenMiddleware::class);
-
     });
-
     $v2024->group("/s/{userId}", function (RouteCollectorProxy $shared) {
-
         $shared->get("/rosters/{rosterId}", [RosterController::class, 'findShared']);
-
     });
-
 });
 
