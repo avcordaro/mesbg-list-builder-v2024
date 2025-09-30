9c61a8cfe2c2a5bb4e91772b5a725097a3cf2dd0
mhollink <hollink.mh@gmail.com>
2025-09-30 13:07:59 +0200

    Fix breadcrumbs on the roster page

--- src/pages/roster/RosterBreadcrumbs.tsx
+++ src/pages/roster/RosterBreadcrumbs.tsx
@@ -1,15 +1,19 @@
 import { Breadcrumbs } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material/styles";
 import { Link } from "../../components/atoms/link/Link.tsx";
 import { useRosterBuildingState } from "../../state/roster-building";
-import { RosterGroup } from "../../state/roster-building/groups";
+import { useThemeContext } from "../../theme/ThemeContext.tsx";
 import { Roster as RosterType } from "../../types/roster.ts";
+import { selectParentGroups, selectRosterGroup } from "../../utils/groups.ts";
 
 export const RosterBreadcrumbs = ({ roster }: { roster: RosterType }) => {
-  const group: RosterGroup | undefined = useRosterBuildingState(
-    (state) =>
-      roster.group && state.groups.find(({ id }) => roster.group === id),
-  );
+  const { palette } = useTheme();
+  const { mode } = useThemeContext();
+
+  const group = useRosterBuildingState(selectRosterGroup(roster));
+  const parents = useRosterBuildingState(selectParentGroups(group));
+
   return (
     <Breadcrumbs sx={{ mb: 1 }}>
       <Link
@@ -20,11 +24,24 @@ export const RosterBreadcrumbs = ({ roster }: { roster: RosterType }) => {
       >
         My Rosters
       </Link>
+      {parents.map((parent) => (
+        <Link
+          key={parent.slug}
+          to={`/rosters/${parent.slug}`}
+          style={{
+            textDecoration: "none",
+            color: mode === "dark" ? palette.info.light : palette.info.main,
+          }}
+        >
+          {parent.name}
+        </Link>
+      ))}
       {group && (
         <Link
           to={`/rosters/${group.slug}`}
           style={{
             textDecoration: "none",
+            color: mode === "dark" ? palette.info.light : palette.info.main,
           }}
         >
           {group.name}
