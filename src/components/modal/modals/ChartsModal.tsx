75e90e8c84a95a5c31e748fd22234d2b9483226b
Jean-Loup Trolliard <jl.trolliard@gmail.com>
2026-02-11 22:25:15 -0500

    Improve To Wound table in modal

--- src/components/modal/modals/ChartsModal.tsx
+++ src/components/modal/modals/ChartsModal.tsx
@@ -1,6 +1,7 @@
 import { DialogContent } from "@mui/material";
 import { useAppState } from "../../../state/app";
 import { useThemeContext } from "../../../theme/ThemeContext.tsx";
+import { ToWoundChartTable } from "./ToWoundChartTable.tsx";
 
 export function ChartsModal() {
   const {
@@ -8,19 +9,25 @@ export function ChartsModal() {
   } = useAppState();
   const { mode } = useThemeContext();
 
+  const isToWoundChart = selectedChart === "to-wound-chart";
+
   return (
     <DialogContent>
       <center>
-        <img
-          src={`${RESOURCES_URL}/images/charts/` + selectedChart + ".png"}
-          alt={selectedChart}
-          style={{
-            maxWidth: "100%",
-            border: "1px solid #6C757D",
-            boxShadow: "0 0 5px 2px #6C757D",
-            filter: mode === "dark" ? "invert(1)" : "",
-          }}
-        />
+        {isToWoundChart ? (
+          <ToWoundChartTable />
+        ) : (
+          <img
+            src={`${RESOURCES_URL}/images/charts/` + selectedChart + ".png"}
+            alt={selectedChart}
+            style={{
+              maxWidth: "100%",
+              border: "1px solid #6C757D",
+              boxShadow: "0 0 5px 2px #6C757D",
+              filter: mode === "dark" ? "invert(1)" : "",
+            }}
+          />
+        )}
       </center>
     </DialogContent>
   );
