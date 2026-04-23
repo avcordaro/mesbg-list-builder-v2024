4b1697d7c5fe88d385817a4138ce91362678322d
mhollink <hollink.mh@gmail.com>
2026-04-23 11:25:38 +0200

    Reduce amount of navigation items

--- src/layout/navigation/menu/useMenuItems.tsx
+++ src/layout/navigation/menu/useMenuItems.tsx
@@ -1,20 +1,17 @@
 import {
-  Announcement,
   AutoAwesome,
   CategoryOutlined,
   Info,
-  OpenInNew,
   Segment,
   Settings,
 } from "@mui/icons-material";
 import BugReportIcon from "@mui/icons-material/BugReport";
 import { AiFillTrophy } from "react-icons/ai";
-import { FaDatabase, FaDiscord, FaPatreon } from "react-icons/fa";
+import { FaDatabase } from "react-icons/fa";
 import { GiSwordsEmblem } from "react-icons/gi";
 import { HiIdentification } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 import { DrawerTypes } from "../../../components/drawer/drawers.tsx";
-import { DISCORD_LINK, PATREON_LINK } from "../../../pages/home/Home.tsx";
 import { useAppState } from "../../../state/app";
 import { useChartMenuItems } from "./useChartMenuItems.tsx";
 import { useRosterMenuItems } from "./useRosterMenuItems.tsx";
@@ -82,18 +79,6 @@ export const useMenuItems = () => {
       active: location.pathname === "/about",
     },
     {
-      icon: <FaDiscord style={{ fontSize: "1.5rem" }} />,
-      label: "MLB Discord",
-      action: () => window.open(DISCORD_LINK, "_blank"),
-      active: false,
-    },
-    {
-      icon: <FaPatreon />,
-      label: "Support us on Patreon",
-      action: () => window.open(PATREON_LINK, "_blank"),
-      active: false,
-    },
-    {
       icon: <BugReportIcon />,
       label: "Report a Bug / Correction",
       action: () =>
@@ -106,26 +91,6 @@ export const useMenuItems = () => {
       label: "App Settings",
       action: () => navigate("/settings"),
       active: location.pathname === "/settings",
-    },
-    { divider: true },
-    {
-      icon: <OpenInNew />,
-      label: "v2018 Edition",
-      action: () =>
-        window.open("https://v2018.mesbg-list-builder.com/", "_blank").focus(),
-      active: false,
-    },
-    {
-      icon: <Announcement />,
-      label: "FAQs and Errata",
-      action: () =>
-        window
-          .open(
-            "https://www.warhammer-community.com/en-gb/downloads/middle-earth-strategy-battle-game/",
-            "_blank",
-          )
-          .focus(),
-      active: false,
     },
   ];
 };
